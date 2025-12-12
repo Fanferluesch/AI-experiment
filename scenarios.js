4d5641bc6174574a01741d9b4ea9e289b167e2c0
trig11 <tr11.rig@gmail.com>
2025-12-12 13:52:02 +0100

    added variable scenario description

---
+++ scenarios.js
@@ -0,0 +1,7 @@
+const application_low_risk = `You are applying for an interesting side job that you think you would be a good fit for, but it is not essential for your financial security. While it would be disappointing if you don't get the job, it wouldn't be problematic. There are several ways to evaluate your application.` 
+
+const application_high_risk = `You are applying for a job that aligns with your career aspirations. You believe that you are a good fit for the position, and you urgently need it to pay your bills. Not getting the job would be devastating for you. There are several ways to evaluate your application.` 
+
+const medical_low_risk = `You have a spot on your arm and are going to the doctor's office to find out if it's a pimple or a rash. It's not life-threatening, but you'd still like to know. You are offered a choice of diagnostic methods.`
+
+const medical_high_risk = `You have a spot dot on your arm and are going to the doctor's office to find out if it's a benign mole or skin cancer. You're scared because it could be life-threatening. You're being offered different ways to be diagnosed.`
